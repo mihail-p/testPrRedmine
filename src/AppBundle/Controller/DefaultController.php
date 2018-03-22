1409e29c14a38618a4dc0ca12ba089cceac78694
Mihail <mishaprk@gmail.com>
2018-03-22 14:39:29 +0200

    Add list of Project

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,6 +9,11 @@ use Redmine\Client;
 
 class DefaultController extends Controller
 {
+    const URL = 'https://redmine.ekreative.com';
+    const API_KEY= '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c';
+    const USER='phptest';
+    const PASS='9uu82T487m6V41G';
+
     /**
      * @Route("/", name="homepage")
      */
@@ -19,18 +24,29 @@ class DefaultController extends Controller
         return $this->render('@App/index.html.twig', array(
             'data' => $data));
     }
+
     /**
      * @Route("/testAPI", name="test_api")
      */
-    public function test(Request $request)
+    public function viewAction(Request $request)
     {
-        $dataAPI='API Data';
-        $client = new Client('https://redmine.ekreative.com', '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c');
-        $client->project->all(['limit' => 10]);
+        $count=0;
+        $listProject = 'API Data';
+        $client = new Client(self::URL, self::USER, self::PASS);// self::API_KEY);
 
-        $dataAPI = $client;
+        $listProject = $client->project->listing();
+        $listIssues = $client->issue->all();
+        foreach ($listIssues['issues'] as $keyI=>$valueI) {
+            foreach ($listProject as $keyP=>$valueP){
+                if ($valueI['project']['id'] == $valueP) {
+                    $listIssuesArr[$valueI['id']] = $valueP; $count++; }
+            }
+        }
+        $dataAPI= $listIssues['issues'];
 
-        return $this->render('@App/testAPI.html.twig',['dataAPI'=> $dataAPI]);
+        return $this->render('@App/testAPI.html.twig', [
+            'listProject' => $listProject, 'listIssues' => $listIssues, 'dataAPI'=> $dataAPI,
+            'listIA'=>$listIssuesArr, 'count'=>$count]);
 
     }
 }
