7ee00200119f59376cd23cd56b5234beeef3501a
Mihail <mishaprk@gmail.com>
2018-03-23 14:23:51 +0200

    Add create Log time to Redmine server

--- src/AppBundle/Controller/TrackTimeController.php
+++ src/AppBundle/Controller/TrackTimeController.php
@@ -4,7 +4,13 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\TrackTime;
 use Redmine\Client;
 
 class TrackTimeController extends Controller
@@ -32,11 +38,45 @@ class TrackTimeController extends Controller
     /**
      * @Route("/new_time_entries/{prId}", name="createTimeEntry")
      */
-    public function newAction($prId)
+    public function newAction(Request $request, $prId)
     {
-        $client = new Client(self::URL, self::USER, self::PASS);// self::API_KEY);
+        $trackTime = new TrackTime();
+        $trackTime->setDate(date("Y-m-d")); /* new \DateTime('today')); */
 
+        $form = $this->createFormBuilder($trackTime)
+            ->add('date', TextType::class)//DateType::class, ['input' => 'string'])
+            ->add('hours', IntegerType::class)
+            ->add('comment', TextType::class)
+            ->add('Activity', ChoiceType::class, ['choices' => [
+                '8' => 'Design',
+                '9' => 'Development',
+                '10' => 'Management',
+                '11' => 'Testing']])
+            ->add('overtime', CheckboxType::class, ['required' => false])
+            ->add('create', SubmitType::class)
+            ->getForm();
 
-        return $this->render('@App/listTrackTime.html.twig', ['trackTime' => $trackTime]);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $client = new Client(self::URL, self::USER, self::PASS);// self::API_KEY);
+            $client->time_entry->create([
+                'project_id' => $prId,
+                'spent_on' => $trackTime->getDate(),
+                'hours' => $trackTime->getHours(),
+                'comments' => $trackTime->getComment(),
+                'activity_id' => $trackTime->getActivity(),
+                'custom_fields' => [
+                    [
+                        'id' => 5,
+                        'name' => 'Overtime',
+                    'value' => $trackTime->getOvertimeInt(),
+                    ],
+                ],
+            ]);
+
+            return $this->render('@App/showTrackTimeData.html.twig', ['timeDate' => $trackTime]);
+        }
+
+        return $this->render('@App/addTrackTime.html.twig', ['form' => $form->createView()]);
     }
 }
---
+++ src/AppBundle/Entity/TrackTime.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 3/23/18
+ * Time: 9:16 AM
+ */
+
+namespace AppBundle\Entity;
+
+
+class TrackTime
+{
+    protected $date;
+    protected $hours;
+    protected $comment;
+    protected $activity;
+    protected $overtime;
+    protected $overtime_int;
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setHours($hours)
+    {
+        $this->hours = $hours;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHours()
+    {
+        return $this->hours;
+    }
+
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setActivity($activity)
+    {
+        $this->activity = $activity;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    public function setOvertime($overtime)
+    {
+        $this->overtime = $overtime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOvertime()
+    {
+        return $this->overtime;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getOvertimeInt()
+    {
+        if ($this->overtime) $this->overtime_int = 1;
+        else $this->overtime_int = 0;
+
+        return $this->overtime_int;
+    }
+}
